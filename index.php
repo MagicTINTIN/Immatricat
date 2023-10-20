e705259ec528124066022042ed21e094b07945a0
MagicTINTIN <valentinservieres@gmail.com>
2023-10-20 16:42:05 +0300

    set css js for buttons

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ if (isset($_POST["end"])) {
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width, height=device-height, target-densitydpi=device-dpi" />
     <title>Immatricat</title>
-
+    <link href="vars.css" rel="stylesheet">
     <link href="styles.css" rel="stylesheet">
     <meta name="author" content="MagicTINTIN">
     <meta name="description" content="J'ai aucun problème avec les statistiques. J'arrête quand je veux.">
@@ -30,34 +30,40 @@ if (isset($_POST["end"])) {
 </head>
 
 <body>
-    <?php
-    if (!isset($_SESSION["name"]) || !isset($_SESSION["time"])) {
-    ?>
-        <form method="post">
-            <input type="text" id="sessioninput" name="newsession" required maxlength="32" placeholder="pseudo" title="Enter a pseudo for your session">
-            <input type="submit" id="sessionsubmit" name="startsession" value="START" />
-        </form>
-    <?php
-    } else {
-    ?>
-        <form method="post">
-            <span id="output"></span>
-            <div id="multibtn">
-                <input type="submit" id="platesubmit" name="sendnplate" value="NORMAL" />
-                <input type="submit" id="platesubmit" name="sendbplate" value="BUS" />
-                <input type="submit" id="platesubmit" name="sendlplate" value="LOCATION" />
-                <input type="submit" id="platesubmit" name="sendpplate" value="PARKED" />
-            </div>
-            <input type="text" id="plateinput" name="plate" required pattern="^[A-Za-z]{3}\d{3}$" oldpattern="[a-zA-Z0-9_-]+" minlength="6" maxlength="6" size="6" title="Enter the car plate" placeholder="XXX000">
+    <main>
+        <?php
+        if (!isset($_SESSION["name"]) || !isset($_SESSION["time"])) {
+        ?>
+            <form id="createsession" method="post">
+                <input type="text" id="sessioninput" name="newsession" required maxlength="32" placeholder="pseudo" title="Enter a pseudo for your session">
+                <input type="submit" id="sessionsubmit" name="startsession" value="START" />
+            </form>
+        <?php
+        } else {
+        ?>
+            <form id="plateform" method="post">
+                <span id="output"></span>
+                <div id="multibtn">
+                    <div class="row">
+                        <input type="submit" id="platesubmitn" class="subplate" name="sendnplate" value="NORMAL" />
+                        <input type="submit" id="platesubmitb" class="subplate" name="sendbplate" value="BUS" />
+                    </div>
+                    <div class="row">
+                        <input type="submit" id="platesubmitl" class="subplate" name="sendlplate" value="LOCATION" />
+                        <input type="submit" id="platesubmitp" class="subplate" name="sendpplate" value="PARKED" />
+                    </div>
+                </div>
+                <input type="text" id="plateinput" name="plate" required pattern="^[A-Za-z]{3}\d{3}$" oldpattern="[a-zA-Z0-9_-]+" minlength="6" maxlength="6" size="6" title="Enter the car plate" placeholder="XXX000">
 
-        </form>
-        <form method="post">
-            <input type="submit" id="submitend" name="end" value="END" />
-        </form>
-    <?php
-    }
-    ?>
-    <script></script>
+            </form>
+            <form id="endform" method="post">
+                <input type="submit" id="submitend" name="end" value="END SESSION" />
+            </form>
+        <?php
+        }
+        ?>
+    </main>
+    <script src="script.js"></script>
 </body>
 
 </html>
