37d7b09a46b99a0cc55975afb4c51c7eb5a5f7f5
MagicTINTIN <valentinservieres@gmail.com>
2023-10-20 19:10:23 +0300

    get previous values

--- index.php
+++ index.php
@@ -11,6 +11,75 @@ if (isset($_POST["end"])) {
     header("Location: ./");
 }
 
+include_once("includes/db.php");
+include_once("includes/time.php");
+$outputvalue = "";
+
+if (isset($_POST["plate"]) && isset($_SESSION["name"]) && isset($_SESSION["time"])) {
+    $platename = htmlspecialchars($_POST["plate"]);
+
+    $db = dbConnect();
+    $platesStatement = $db->prepare('SELECT * FROM plates WHERE username = :plate');
+    $platesStatement->execute(['plate' => $platename]);
+    $plates = $platesStatement->fetchAll();
+
+
+    $isNewPlate = 1;
+    $newplatetype = 0;
+    $oldplatetype = 0;
+    $nbSeen = 1;
+    $time = time();
+    if (sizeof($plates) > 0) {
+        $isNewPlate = false;
+        $oldplatetype = $plates[0]["type"];
+        $nbSeen += $plates[0]["nbSeen"];
+        $outputvalue += "+ Seen ". getTime((int) $plates[0]["lastSeen"], $time) ." ago<br>";
+    } else {
+        $outputvalue += "+ NEW <br>";
+    }
+
+    if (isset($_POST["sendnplate"])) {
+        // do nothing
+    } else if (isset($_POST["sendlplate"])) {
+        if ($oldplatetype == 7)
+            $outputvalue += "-> Rented car<br>";
+        $newplatetype = 7;
+    } else if (isset($_POST["sendbplate"])) {
+        if ($oldplatetype == 5)
+            $outputvalue += "-> Bus<br>";
+        $newplatetype = 5;
+    } else if (isset($_POST["sendpplate"])) {
+        if ($oldplatetype == 3)
+        {
+            $isNewPlate = -1;
+            $outputvalue += "ALREADY PARKED<br>";
+        }
+        else
+            $outputvalue += "-> Parked<br>";
+        $newplatetype = 3;
+    }
+    if (!isset($_POST["sendpplate"]) && $oldplatetype == 3) {
+        $outputvalue += "-> Not parked<br>";
+    }
+
+
+    if ($isNewPlate == 1) {
+        $sqlQuery = 'INSERT INTO plates(session, createdAt, plate, lastSeen, nbSeen, type) VALUES (:usr, :mail, :pw)';
+
+        $insertUser = $db->prepare($sqlQuery);
+        $insertUser->execute([
+            'usr' => htmlspecialchars($_POST['cauthor']),
+            'mail' => htmlspecialchars($_POST['cemail']),
+            'pw' => password_hash(htmlspecialchars($_POST['cpassword']), PASSWORD_DEFAULT),
+        ]);
+        $_SESSION["username"] = htmlspecialchars($_POST['cauthor']);
+        $_SESSION["email"] = htmlspecialchars($_POST['cemail']);
+    }
+    else if ($isNewPlate == 0) {
+
+    }
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -59,8 +128,11 @@ if (isset($_POST["end"])) {
                 <input type="submit" id="submitend" name="end" value="END SESSION" />
             </form>
 
-            <div id="output">Last seen 3d ago</div>
+            <?php if ($outputvalue != "") {
+            ?>
+                <div id="output"><?php echo $outputvalue ?></div>
         <?php
+            }
         }
         ?>
     </main>
